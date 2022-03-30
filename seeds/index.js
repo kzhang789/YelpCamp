0d5188adf0d4ee97ce216ef529519256eee0fc09
Kailai Zhang <y54078@gmail.com>
2022-03-30 16:59:57 -0700

    deployed version

--- seeds/index.js
+++ seeds/index.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 const cities = require('./cities');
 const {places, descriptors} = require('./seedHelpers');
-const Campground = require('../models/campground')
+const Campground = require('../models/campground');
+const dbUrl = 'mongodb://localhost:27017/yelp-camp';
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(dbUrl, {useNewUrlParser: true, useUnifiedTopology: true});
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
@@ -15,11 +16,12 @@ const sample = array => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
     await Campground.deleteMany({});
-    for (let i = 0; i < 300; i++) {
+    for (let i = 0; i < 20; i++) {
         const rand1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
-            author: '623b8e07483608afedda0d34',
+            author: '623b8e07483608afedda0d34', 
+            //author: '62422014ad295031f78ed59f',
             location: `${cities[rand1000].city}, ${cities[rand1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             //image:'https://source.unsplash.com/collection/483251',
